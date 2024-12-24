52fb4e2927f5dcf0b5ddfed4866e964135c6b5bc
Faizan Fazul <faizanfazul6@gmail.com>
2024-12-24 20:59:34 +0500

    ui: reposition product description to the bottom of the page

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -263,13 +263,6 @@ function ProductPage() {
               <span className="product-actual-price">PKR {product.Price}</span>
             )}
           </div>
-          <div className="product-desc">
-            <span>
-              {i18n.language === "ur"
-                ? product.DescriptionInUr
-                : product.Description}
-            </span>
-          </div>
           <span className="divider"></span>
           {product?.Colors?.[0] !== "" && (
             <div className="product-colors">
@@ -354,6 +347,31 @@ function ProductPage() {
                   stroke="black"
                   strokeWidth="1.5"
                   strokeLinecap="round"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="product-cart-concern">
+            <button className="add-to-cart-btn" onClick={handleAddToCartItem}>
+              {t("Add_To_Cart")}
+            </button>
+            <button className="buy-now-btn" onClick={handleBuyNowItem}>
+              {t("Buy_Now")}
+            </button>
+            <button className="add-wish-btn" onClick={handleWishlistToggle}>
+              <svg
+                width="22"
+                height="20"
+                viewBox="0 0 22 20"
+                fill={heart ? "#000" : "none"}
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6 1C3.239 1 1 3.216 1 5.95C1 8.157 1.875 13.395 10.488 18.69C10.6423 18.7839 10.8194 18.8335 11 18.8335C11.1806 18.8335 11.3577 18.7839 11.512 18.69C20.125 13.395 21 8.157 21 5.95C21 3.216 18.761 1 16 1C13.239 1 11 4 11 4C11 4 8.761 1 6 1Z"
+                  stroke="black"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
             </button>
@@ -557,30 +575,12 @@ function ProductPage() {
               <span>{t("Free_Delivery_label")}</span>
             </div>
           )}
-          <div className="product-cart-concern">
-            <button className="add-to-cart-btn" onClick={handleAddToCartItem}>
-              {t("Add_To_Cart")}
-            </button>
-            <button className="buy-now-btn" onClick={handleBuyNowItem}>
-              {t("Buy_Now")}
-            </button>
-            <button className="add-wish-btn" onClick={handleWishlistToggle}>
-              <svg
-                width="22"
-                height="20"
-                viewBox="0 0 22 20"
-                fill={heart ? "#000" : "none"}
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M6 1C3.239 1 1 3.216 1 5.95C1 8.157 1.875 13.395 10.488 18.69C10.6423 18.7839 10.8194 18.8335 11 18.8335C11.1806 18.8335 11.3577 18.7839 11.512 18.69C20.125 13.395 21 8.157 21 5.95C21 3.216 18.761 1 16 1C13.239 1 11 4 11 4C11 4 8.761 1 6 1Z"
-                  stroke="black"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </button>
+          <div className="product-desc">
+            <span>
+              {i18n.language === "ur"
+                ? product.DescriptionInUr
+                : product.Description}
+            </span>
           </div>
         </div>
       </div>
