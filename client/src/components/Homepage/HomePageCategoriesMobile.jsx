2877f27a1db489bcfb5736720f1a97546822a8b2
Faizan Fazul <faizanfazul6@gmail.com>
2025-01-14 10:29:36 +0500

    feat: Home Page subCategories in Mobile view fix: categories card button on mobile view

--- client/src/components/Homepage/HomePageCategoriesMobile.jsx
+++ client/src/components/Homepage/HomePageCategoriesMobile.jsx
@@ -2,26 +2,57 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 function HomePageCategoriesMobile({ categories }) {
   const { i18n } = useTranslation();
+  const navigate = useNavigate();
   return (
     <Swiper
       className="home-page-categories-mobile"
       spaceBetween={5}
-      slidesPerView={4}
-      loop={true}
+      slidesPerView={3}
+      loop={categories?.length > 3}
       modules={[Navigation]}
       navigation
     >
       {categories.map((category, index) => (
         <SwiperSlide key={index}>
-          <Link to={"/collection/" + category.Name}>
-            {i18n.language === "ur" ? category.NameInUr : category.Name}
-          </Link>
+          <select
+            onChange={(e) => {
+              if (e.target.value) {
+                navigate("/collection/" + e.target.value);
+              }
+            }}
+          >
+            <option value="" key={category.Name + "label"}>
+              {i18n.language === "ur"
+                ? "-- " + category.NameInUr + " --"
+                : "-- " + category.Name + " --"}
+            </option>
+            {i18n.language === "ur" ? (
+              <option value={category.Name} key={category.Name}>
+                &#8592; {category.NameInUr}
+              </option>
+            ) : (
+              <option value={category.Name} key={category.Name}>
+                {category.Name} &#8594;
+              </option>
+            )}
+            {i18n.language === "ur"
+              ? category?.SubCategoriesInUr?.map((subCategoryUr) => (
+                  <option value={subCategoryUr} key={subCategoryUr}>
+                    &#8592; {subCategoryUr}
+                  </option>
+                ))
+              : category?.SubCategories?.map((subCategory) => (
+                  <option value={subCategory} key={subCategory}>
+                    {subCategory} &#8594;
+                  </option>
+                ))}
+          </select>
         </SwiperSlide>
       ))}
     </Swiper>
