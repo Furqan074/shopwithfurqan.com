c7ca9891356fe99c221bfa3c7bfa43f7d1537580
Faizan Fazul <faizanfazul6@gmail.com>
2025-04-07 22:58:27 +0500

    fix: postcss plugin issues

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,3 @@
-import "./index.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import ProductsTable from "./components/ProductsTable";
